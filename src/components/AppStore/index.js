e0a1d020b66e40cd00a011f9ebc37a62c0219463
Haridokku <124425442+Haridokku@users.noreply.github.com>
2024-02-17 10:22:26 +0530

    Update index.js

--- src/components/AppStore/index.js
+++ src/components/AppStore/index.js
@@ -1,4 +1,41 @@
-import {Component} from 'react'
+import './index.css'
+
+const ContactItem = props => {
+  const {contactDetails, toggleIsFavorite} = props
+  const {name, mobileNo, isFavorite, id} = contactDetails
+
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  const onChangeFavoriteIcon = () => {
+    toggleIsFavorite(id)
+  }
+
+  return (
+    <li className="table-row">
+      <div className="table-cell name-column">
+        <p>{name}</p>
+      </div>
+      <hr className="separator" />
+      <div className="table-cell mobile-no-column">
+        <p className="mobile-no-value">{mobileNo}</p>
+        <button
+          type="button"
+          className="favorite-icon-container"
+          onClick={onChangeFavoriteIcon}
+        >
+          <img src={starImgUrl} className="favorite-icon" alt="star" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ContactItem
+
+
+/*import {Component} from 'react'
 
 import AppItem from '../AppItem'
 import TabItem from '../TabItem'
@@ -373,4 +410,4 @@ class AppStore extends Component {
   }
 }
 
-export default AppStore
+export default AppStore */
